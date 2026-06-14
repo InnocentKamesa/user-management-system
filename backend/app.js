6cccea6ac5a4a9803a4b572cbabf7c439d2dd00c
Innocent _kamesa <innocentkamesa05@gmail.com>
2026-06-14 10:12:49 +0200

    Add Vercel app URL to CORS origins

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
+    origin: ["http://127.0.0.1:5173", "http://localhost:5173", "https://kamesa-user-management.vercel.app"],
     credentials: true,
   }),
 );
